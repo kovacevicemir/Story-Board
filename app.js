610a83903bda0aa6c8e70cd354671044112f70e3
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-28 14:29:06 +1000

    passport and google auth 2.0 implementation

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const morgan = require('morgan')
 const connectDB = require('./config/db')
 const exphbs = require('express-handlebars')
 const path = require('path')
+const passport = require('passport')
+const session = require('express-session')
 
 // Load config file
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,11 +26,23 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout:'main' ,extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// session middleware
+app.use(session({
+    secret:'keyboard cat',
+    resave:false,
+    saveUninitialized:false
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
