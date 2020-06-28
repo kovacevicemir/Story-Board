da6cf2711aa06d95254ee6e3ec4bdfc368517754
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-28 12:58:08 +1000

    express router implementation and handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
+const exphbs = require('express-handlebars')
 
 // Load config file
 dotenv.config({path: './config/config.env'})
@@ -9,6 +11,19 @@ connectDB()
 
 const app = express()
 
+//Logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout:'main' ,extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Routes
+app.use('/', require('./routes/index'))
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in
+ ${process.env.NODE_ENV} mode on port ${PORT}`))
