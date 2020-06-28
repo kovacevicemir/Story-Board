d062d7229013ae4b0c24e66376afba62558b40d1
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-28 16:25:38 +1000

    adding: morgan, handlebar helpers, create story get/post, story model...

--- app.js
+++ app.js
@@ -19,13 +19,21 @@ connectDB()
 
 const app = express()
 
+//Body parser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
 //Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+
+//Handlebars helpers
+const {formatDate} = require('./helpers/hbs')
+
 //Handlebars
-app.engine('.hbs', exphbs({defaultLayout:'main' ,extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers:{formatDate} ,defaultLayout:'main' ,extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // session middleware
@@ -46,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 5000
 
